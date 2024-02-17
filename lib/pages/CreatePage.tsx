936da1ccebc3b5ef43df9ed7775d76ae417f1227
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2024-02-17 19:03:24 +0900

    Add login link for existing users

--- lib/pages/CreatePage.tsx
+++ lib/pages/CreatePage.tsx
@@ -12,6 +12,10 @@ export default function CreatePage({ error }: { error?: string }) {
 				class="mx-auto mt-4"
 			>
 				<AuthenticationFields />
+				<p class="text-muted">
+					If you already have an account, you can <a href="/login">login</a>{" "}
+					here.
+				</p>
 				{error && <AlertError>{error}</AlertError>}
 				<button type="submit" class="btn btn-primary">
 					Create
